d9711d3622c967c3e69aceabc6950f7ba020d73f
Uchchash346 <mru416@gmail.com>
2021-08-09 21:44:30 +0600

    statusbar setup

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+
+console.log(StatusBar.currentHeight);
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+
 });
