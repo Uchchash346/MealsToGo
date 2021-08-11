44cebb130f0e4db355234585c25c79356d7a545a
Uchchash346 <mru416@gmail.com>
2021-08-11 10:54:12 +0600

    add search bar

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
+import { Searchbar } from 'react-native-paper';
 
 console.log(StatusBar.currentHeight);
 export default function App() {
   return (
     <>
       <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
-          <Text>
-            search 1234
-          </Text>
+        <View style={styles.search}>
+          <Searchbar />
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
@@ -23,5 +21,12 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  }
 });
