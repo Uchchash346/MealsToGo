35509c29a5d65006bf38ecda3d0b6668ad2b6d02
Uchchash346 <mru416@gmail.com>
2021-08-13 11:45:20 +0600

    restaurant info setup

--- src/features/restaurents/components/restaurant-info.components.js
+++ src/features/restaurents/components/restaurant-info.components.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
 
- export const RestaurantInfo = () => <Text>Restaurant Info</Text>
+export const RestaurantInfo = ({ restaurant = {} }) => (
+    const {
+    name = 'Some restaurant',
+    icon,
+    photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"],
+    address = "100 some random streets",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+} = restaurant;
+return <Text>{name}</Text>
+ );
