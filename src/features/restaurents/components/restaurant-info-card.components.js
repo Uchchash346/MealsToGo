2167a39a831813b21860164e3bbb25ee4a5abebd
Uchchash346 <mru416@gmail.com>
2021-08-13 16:32:36 +0600

    rename filename

--- src/features/restaurents/components/restaurant-info-card.components.js
+++ src/features/restaurents/components/restaurant-info-card.components.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Card } from 'react-native-paper';
 
-export const RestaurantInfo = ({ restaurant = {} }) => {
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
         name = 'Some restaurant',
         icon,
@@ -17,7 +17,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     return (
         <Card elevation={5} style={styles.card}>
             <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-            <Text>{name}</Text>
+            <Text style={styles.title}>{name}</Text>
         </Card>
     );
 };
@@ -25,4 +25,5 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
 const styles = StyleSheet.create({
     card: { backgroundColor: "white" },
     cover: { padding: 20, backgroundColor: "white" },
+    title: { padding: 16 },
 })
